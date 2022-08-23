d844ab5deba408830d539915538ff557aa36278e
AlexSna <asnatschko@gmail.com>
2022-08-23 11:24:43 +0200

    changing some files

--- index.js
+++ index.js
@@ -10,6 +10,38 @@ import toggleAnswer from "./js/_answer.js";
 
 //import { showAnswer1, showAnswer2, showAnswer3 } from "./js/_answer.js";
 
+function initPages() {
+    const pages = ["indexPage", "bookmarkPage"]
+    pages.forEach((pageId) => {
+        let element = document.querySelector("#" + pageId)
+        element.style.display = "none"
+    })
+}
+
+function switchPage(pageId) {
+    initPages()
+    let element = document.querySelector("#" + pageId)
+    element.style.display = ""
+}
+
+function initPageButtons() {
+    const buttons = {
+        "indexPageButton": "indexPage", 
+        "bookmarkPageButton": "bookmarkPage"
+    }
+    Object.entries(buttons).forEach(([buttonId, pageId]) => {
+        let element = document.querySelector("#" + buttonId)
+        element.addEventListener("click", (e) => {
+            console.log(e)
+            e.preventDefault();
+            switchPage(pageId)
+            element.classList.add("active")
+        })
+    })
+}
+
+switchPage("indexPage")
+initPageButtons()
 
 toggleBookmark();
 toggleAnswer();
